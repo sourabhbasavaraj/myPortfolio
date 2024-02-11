490a960cc2c67ff8275bbeee7596e0fdcd9e11e6
SourabhBS <sourabhbasavaraj20@gmail.com>
2024-02-11 01:02:45 -0600

    Version-1

--- app/components/heroSection.jsx
+++ app/components/heroSection.jsx
@@ -4,11 +4,11 @@ import Image from 'next/image';
 const HeroSection = () => {
   return (
     <section>
-        <div className="grid grid-cols-1 lg:grid-cols-12">
-            
-          <div className="col-span-7 place-self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello, I am Sourabh Sajjan.</h1>
-            <p className="text-red-300 text-lg lg:text-xl mb-6">
+        <div className="grid grid-cols-1 sm:grid-cols-12">
+
+          <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">Hello, I am Sourabh Sajjan.</h1>
+            <p className="text-red-300 text-base sm:text-lg lg:text-xl mb-6">
                 As a Software Development Engineer (SDE), I specialize in crafting dynamic applications 
                 across frontend, backend, and full-stack environments. With expertise in access 
                 management using Sailpoint and recent engagements in ServiceNow, I excel in scripting 
@@ -16,6 +16,10 @@ const HeroSection = () => {
                 Illinois Institute of Technology, I am committed to pushing the boundaries of
                 technology and innovation.
             </p>
+            <div>
+                <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-white text-white">Hire Me</button>
+                <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border mt-3 border-white" >Download Resume</button>
+            </div>
           </div>
 
           <div className="col-span-5 place-self-center mt-4 lg:mt-0">
--- app/page.js
+++ app/page.js
@@ -3,8 +3,11 @@ import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-black items-center justify-between p-24 container mx-auto px-12">
-      <HeroSection />
+    <main className="flex min-h-screen flex-col bg-black items-center justify-between p-24 ">
+      <div className="container mx-auto px-12"> 
+        <HeroSection />
+      </div>
+    
     </main>
   );
 }
